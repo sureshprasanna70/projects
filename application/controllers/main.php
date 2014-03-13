7bfc38e7b623f2c6f80b703f53684236a2a15a39
sureshprasanna70 <sureshprasannaceg@gmail.com>
2014-03-13 21:13:20 +0530

    finshed route config

---
+++ application/controllers/main.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+Class Main extends CI_Controller
+{
+	public function index()
+	{
+		echo "hello";
+	}
+	public function contact()
+	{
+		echo "contact";
+	}
+}
